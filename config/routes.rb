6d2943968d8b5cd07dc2bf1784d370381533fd22
EC2 Default User <ec2-user@ip-172-31-31-33.ec2.internal>
2022-09-10 12:24:50 +0000

    顧客側スタート

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
- 
+
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
